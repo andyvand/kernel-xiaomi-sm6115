fe81b2c0f16bb02dd0ca357141e246e68f4db97a
Deepak Katragadda <dkatraga@codeaurora.org>
2016-09-29 13:28:59 +0530

    clk: qcom: Add support for hardware control branch clocks
    Add new ops to allow clock clients to enable/disable hardware
    dynamic gating of the clock branch.
    
    Change-Id: I14abed3827de8cefc31f3deb3c1e589136c32b8d
    Signed-off-by: Taniya Das <tdas@codeaurora.org>
    Signed-off-by: Deepak Katragadda <dkatraga@codeaurora.org>
    Signed-off-by: David Dai <daidavid1@codeaurora.org>

--- drivers/clk/qcom/clk-branch.h
+++ drivers/clk/qcom/clk-branch.h
@@ -39,6 +39,7 @@ struct clk_branch {
 
 extern const struct clk_ops clk_branch_ops;
 extern const struct clk_ops clk_branch2_ops;
+extern const struct clk_ops clk_branch2_hw_ctl_ops;
 extern const struct clk_ops clk_branch_simple_ops;
 
 #define to_clk_branch(_hw) \

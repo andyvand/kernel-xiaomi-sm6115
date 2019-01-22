070bca6451a79b94b0108aadac497f9954fc6622
Sahil Chandna <chandna@codeaurora.org>
2019-01-22 12:16:07 +0530

    power: smb5: Wireless charger removal detection
    When POWER_SUPPLY_PROP_DC_RESET is set, trigger the
    sequence for detection of wireless charger removal.
    
    Change-Id: Ic5a9405c1acb39df29f8dba837f60660bae4fad3
    Signed-off-by: Sahil Chandna <chandna@codeaurora.org>
    Signed-off-by: Guru Das Srinagesh <gurus@codeaurora.org>

--- drivers/power/supply/qcom/qpnp-smb5.c
+++ drivers/power/supply/qcom/qpnp-smb5.c
@@ -1288,6 +1288,7 @@ static enum power_supply_property smb5_dc_props[] = {
 	POWER_SUPPLY_PROP_VOLTAGE_MAX,
 	POWER_SUPPLY_PROP_INPUT_VOLTAGE_REGULATION,
 	POWER_SUPPLY_PROP_REAL_TYPE,
+	POWER_SUPPLY_PROP_DC_RESET,
 };
 
 static int smb5_dc_get_prop(struct power_supply *psy,
@@ -1322,6 +1323,8 @@ static int smb5_dc_get_prop(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_INPUT_VOLTAGE_REGULATION:
 		rc = smblib_get_prop_voltage_wls_output(chg, val);
+	case POWER_SUPPLY_PROP_DC_RESET:
+		val->intval = 0;
 		break;
 	default:
 		return -EINVAL;
@@ -1351,6 +1354,9 @@ static int smb5_dc_set_prop(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_INPUT_VOLTAGE_REGULATION:
 		rc = smblib_set_prop_voltage_wls_output(chg, val);
+		break;
+	case POWER_SUPPLY_PROP_DC_RESET:
+		rc = smblib_set_prop_dc_reset(chg);
 		break;
 	default:
 		return -EINVAL;

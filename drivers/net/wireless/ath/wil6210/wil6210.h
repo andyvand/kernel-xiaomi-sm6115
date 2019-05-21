a59f9f9e4a81e5d45c414e69acc717111beebc15
Maya Erez <merez@codeaurora.org>
2019-05-21 10:12:01 +0300

    wil6210: add support for spi slave reset
    As SPI core is not being reset in SW reset, FW needs to disable it
    before the reset, to ensure proper SPI re-initialization after the
    reset.
    Add a new WMI command to notify FW to reset SPI slave before
    performing SW reset.
    
    Change-Id: I12d23560dbb28daa64cbebec155847fe32625b63
    Signed-off-by: Maya Erez <merez@codeaurora.org>

--- drivers/net/wireless/ath/wil6210/wil6210.h
+++ drivers/net/wireless/ath/wil6210/wil6210.h
@@ -1484,5 +1484,6 @@ int wmi_addba_rx_resp_edma(struct wil6210_priv *wil, u8 mid, u8 cid,
 			   u16 agg_wsize, u16 timeout);
 
 void update_supported_bands(struct wil6210_priv *wil);
+int wmi_reset_spi_slave(struct wil6210_priv *wil);
 
 #endif /* __WIL6210_H__ */

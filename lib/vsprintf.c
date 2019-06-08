45a1f530f5c37afe3bdd27d383babd4cc18529e5
Patrick Daly <pdaly@codeaurora.org>
2019-06-07 17:00:54 -0700

    lib/vsprintf: Less restrictive hashed pointer printing
    Commit ad67b74d2469 ("printk: hash addresses printed with %p") and
    Commit ef0010a30935 ("vsprintf: don't use 'restricted_pointer()'
    when not restricting") effectively removed the ability to display
    kernel addresses in the kernel log. While this may be a useful feature
    in production builds, it is undesirable when trying to debug.
    
    %px is not a possible alternative, because it is unable to differentiate
    between a debug and production build.
    
    Change-Id: I139fae7b8488936d214efdd2b5b807fa1c005467
    Signed-off-by: Patrick Daly <pdaly@codeaurora.org>

--- lib/vsprintf.c
+++ lib/vsprintf.c
@@ -1968,7 +1968,8 @@ char *pointer(const char *fmt, char *buf, char *end, void *ptr,
 			return buf;
 		}
 	case 'K':
-		if (!kptr_restrict)
+		if (!kptr_restrict ||
+		    IS_ENABLED(CONFIG_DEBUG_CONSOLE_UNHASHED_POINTERS))
 			break;
 		return restricted_pointer(buf, end, ptr, spec);
 	case 'N':
@@ -1999,6 +2000,9 @@ char *pointer(const char *fmt, char *buf, char *end, void *ptr,
 	case 'x':
 		return pointer_string(buf, end, ptr, spec);
 	}
+
+	if (IS_ENABLED(CONFIG_DEBUG_CONSOLE_UNHASHED_POINTERS))
+		return pointer_string(buf, end, ptr, spec);
 
 	/* default is to _not_ leak addresses, hash before printing */
 	return ptr_to_id(buf, end, ptr, spec);
